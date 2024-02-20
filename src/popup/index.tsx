62bfdc730e0abf54cb4eaac032d26b1d75a9d3c4
kaze-k <kerkaze@outlook.com>
2024-02-20 17:43:39 +0800

    chore(popup): 文件重命名

--- src/popup/index.tsx
+++ src/popup/index.tsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client"
 import "~/fontawesome"
 import "~/iconfont.css"
 
-import Popup from "./popup"
+import Popup from "./Popup"
 import "./popup.scss"
 
 const root = createRoot(document.getElementById("root") as HTMLElement)
