56d283af4bd56f8d4c8e8936c1e8c6f2dfa4d1f8
kaze-k <kerkaze@outlook.com>
2024-06-19 15:55:57 +0800

    refactor(views): 简化页面组件代码

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,81 +1,24 @@
-import React, { useEffect, useState } from "react"
-import { useDispatch, useSelector } from "react-redux"
+import type React from "react"
 
-import HomeContent from "~/Layout/Contents/HomeContent"
+import { HomeContent } from "~/Layout/Contents"
 import { HomeHeader } from "~/Layout/Headers"
-import SelectBar from "~/Layout/SelectBar"
-import { useMessage } from "~/components/common/Message"
-import Page from "~/components/common/Page"
-import { clearData, update } from "~/store/features/data"
-import { abort } from "~/store/features/data"
+import SelectBar from "~/components/SelectBar"
+import { useFetchData, useOnlineStatus } from "~/hooks"
 
 /**
  * @description 首页组件
  * @return {*}  {React.ReactElement}
  */
 function Home(): React.ReactElement {
-  const dispatch: Dispatch = useDispatch()
-  const message: ReturnType<typeof useMessage> = useMessage()
-
-  // 状态
-  const darkMode: boolean = useSelector((state: State): boolean => state.theme.darkMode)
-  const isError: boolean = useSelector((state: State): boolean => state.data.isError)
-  const isLoading: boolean = useSelector((state: State): boolean => state.data.isLoading)
-  const [id] = useState<string>(crypto.randomUUID())
-
-  /**
-   * @description 监听在线: 当网络在线时，发送更新信息通信
-   */
-  const onlineListener: () => void = (): void => {
-    if (window.navigator.onLine) {
-      dispatch(update())
-    }
-  }
-
-  // 首次挂载时: 发生更新信息通信/启用存储
-  // 卸载时: 禁用存储/清除数据
-  useEffect((): (() => void) => {
-    dispatch(update())
-
-    return (): void => {
-      dispatch(abort())
-      dispatch(clearData())
-    }
-  }, [])
-
-  // 当网络状态改变时: 监听在线/监听离线
-  useEffect((): (() => void) => {
-    window.addEventListener("online", onlineListener)
-
-    return (): void => {
-      window.removeEventListener("online", onlineListener)
-    }
-  }, [window.navigator.onLine])
-
-  // 当错误状态改变时/当加载状态改变时: 改变消息提示
-  useEffect(() => {
-    if (isLoading) {
-      message.loading(null, { id })
-      return
-    }
-
-    if (isError) {
-      message("更新失败", { id })
-      return
-    }
-
-    if (isError !== null && isLoading !== null && !isError && !isLoading) {
-      message("时间表信息已更新", { id, duration: 2000 })
-      return
-    }
-  }, [isError, isLoading])
+  useFetchData()
+  useOnlineStatus()
 
   return (
-    <Page darkMode={darkMode}>
-      <HomeHeader darkMode={darkMode} />
-      <HomeContent darkMode={darkMode} />
-      <SelectBar darkMode={darkMode} />
-    </Page>
+    <>
+      <HomeHeader />
+      <HomeContent />
+      <SelectBar />
+    </>
   )
 }
 
--- src/views/Setup/index.tsx
+++ src/views/Setup/index.tsx
@@ -1,31 +1,18 @@
-import React from "react"
-import { useSelector } from "react-redux"
+import type React from "react"
 
-import SetupContent from "~/Layout/Contents/SetupContent"
+import { SetupContent } from "~/Layout/Contents"
 import { SetupHeader } from "~/Layout/Headers"
-import { NoticeRow, ResetRow, StorageRow, ThemeRow } from "~/Layout/Rows"
-import Version from "~/components/Version"
-import Page from "~/components/common/Page"
 
 /**
  * @description 设置页组件
  * @return {*}  {React.ReactElement}
  */
 function Setup(): React.ReactElement {
-  // 状态
-  const darkMode: boolean = useSelector((state: State): boolean => state.theme.darkMode)
-
   return (
-    <Page darkMode={darkMode}>
-      <SetupHeader darkMode={darkMode} />
-      <SetupContent darkMode={darkMode}>
-        <NoticeRow darkMode={darkMode} />
-        <ThemeRow darkMode={darkMode} />
-        <StorageRow darkMode={darkMode} />
-        <ResetRow darkMode={darkMode} />
-        <Version />
-      </SetupContent>
-    </Page>
+    <>
+      <SetupHeader />
+      <SetupContent />
+    </>
   )
 }
 
