60c2629df9e22d6031963f4dd9c258689ff2186c
kaze-k <kerkaze@outlook.com>
2024-07-26 05:46:15 +0800

    refactor(components): 删除多余的loading动画

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -5,56 +5,6 @@ import type React from "react"
 import style from "./style.module.scss"
 import LoadingProps from "./types"
 
-// 加载动画
-const LOADING_ICON: React.ReactElement = (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="1em"
-    height="1em"
-    viewBox="0 0 24 24"
-  >
-    <circle
-      cx="4"
-      cy="12"
-      r="1.5"
-      fill="currentColor"
-    >
-      <animate
-        attributeName="r"
-        dur="0.75s"
-        repeatCount="indefinite"
-        values="1.5;3;1.5"
-      ></animate>
-    </circle>
-    <circle
-      cx="12"
-      cy="12"
-      r="3"
-      fill="currentColor"
-    >
-      <animate
-        attributeName="r"
-        dur="0.75s"
-        repeatCount="indefinite"
-        values="3;1.5;3"
-      ></animate>
-    </circle>
-    <circle
-      cx="20"
-      cy="12"
-      r="1.5"
-      fill="currentColor"
-    >
-      <animate
-        attributeName="r"
-        dur="0.75s"
-        repeatCount="indefinite"
-        values="1.5;3;1.5"
-      ></animate>
-    </circle>
-  </svg>
-)
-
 /**
  * @description 加载动画组件
  * @param {LoadingProps} props 加载动画组件props
@@ -64,29 +14,20 @@ const LOADING_ICON: React.ReactElement = (
 function Loading(props: LoadingProps, ref: React.Ref<HTMLDivElement>): React.ReactElement {
   const { icon = "spinner" } = props
 
-  if (icon === "loading") {
+  if (icon === "spinner") {
     return (
       <div
         ref={ref}
-        className={style["loading"]}
+        className={style["spinner"]}
       >
-        {LOADING_ICON}
+        <FontAwesomeIcon
+          icon="spinner"
+          size="2xl"
+          spin
+        />
       </div>
     )
   }
-
-  return (
-    <div
-      ref={ref}
-      className={style["spinner"]}
-    >
-      <FontAwesomeIcon
-        icon="spinner"
-        size="2xl"
-        spin
-      />
-    </div>
-  )
 }
 
 export default forwardRef(Loading)
